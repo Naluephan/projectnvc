e4110b84fe95dac6a8dabc0370aed353868bf968
Warut <mercenary.warut186@gmail.com>
2024-03-05 17:19:01 +0700

    Update EmployeeSalaryRepository
    Changed payslip browsing time to startDate & endDate

--- app/Repositories/Impl/EmployeeSalaryRepository.php
+++ app/Repositories/Impl/EmployeeSalaryRepository.php
@@ -16,11 +16,15 @@ class EmployeeSalaryRepository extends MasterRepository implements EmployeeSalar
         parent::__construct($model);
     }
     public function employeeSalary($param){
-        $data = $this->model->where([
-            ['emp_id' ,'=', $param['emp_id']],
-            ['month' ,'=', $param['month']],
-            ['year' ,'=', $param['year']],
-        ])->get();
+        // $data = $this->model->where([
+        //     ['emp_id' ,'=', $param['emp_id']],
+        //     ['month' ,'=', $param['month']],
+        //     ['year' ,'=', $param['year']],
+        // ])->get();
+        $data = $this->model->whereRaw("CONCAT(YEAR, '-', LPAD(`month`, 2, '0')) BETWEEN ? AND ?", [$param['startMonth'], $param['endMonth']])
+        ->orderBy('YEAR')
+        ->orderBy('month')
+        ->get();
         return $data;
     }
     
