10668d0edeeeb2258c80db3342f43c0cc1f737c0
RATCHAROTE ANGTHONG <68987790+Lechiana@users.noreply.github.com>
2024-04-23 11:52:28 +0700

    Edit API Update Private Car

--- app/Http/Controllers/APIs/PrivateCarController.php
+++ app/Http/Controllers/APIs/PrivateCarController.php
@@ -61,27 +61,27 @@ class PrivateCarController extends Controller
         return $result;
     }
 
-    public function update(Request $request)
-    {
-        $data = $request->all();
-        try {
+    // public function update(Request $request)
+    // {
+    //     $data = $request->all();
+    //     try {
 
-            if ($request->file('car_image')) {
-                $data['car_image'] = save_image($request->file('car_image'), 500, '/images/content/privateCar/');
-            }
-            $this->privateCarRepository->update($data['id'], $data);
+    //         if ($request->file('car_image')) {
+    //             $data['car_image'] = save_image($request->file('car_image'), 500, '/images/content/privateCar/');
+    //         }
+    //         $this->privateCarRepository->update($data['id'], $data);
 
-            $result['status'] = ApiStatus::list_private_car_success_status;
-            $result['statusCode'] = ApiStatus::list_private_car_success_statusCode;
-            $result['listPrivateCar'] = 'Update Successfully.';
-        } catch (\Exception $e) {
-            $result['status'] = ApiStatus::list_private_car_error_statusCode;
-            $result['errCode'] = ApiStatus::list_private_car_error_status;
-            $result['errDesc'] = ApiStatus::list_private_car_errDesc;
-            $result['message'] = $e->getMessage();
-        }
-        return $result;
-    }
+    //         $result['status'] = ApiStatus::list_private_car_success_status;
+    //         $result['statusCode'] = ApiStatus::list_private_car_success_statusCode;
+    //         $result['listPrivateCar'] = 'Update Successfully.';
+    //     } catch (\Exception $e) {
+    //         $result['status'] = ApiStatus::list_private_car_error_statusCode;
+    //         $result['errCode'] = ApiStatus::list_private_car_error_status;
+    //         $result['errDesc'] = ApiStatus::list_private_car_errDesc;
+    //         $result['message'] = $e->getMessage();
+    //     }
+    //     return $result;
+    // }
 
     public function deleteUpdate(Request $request)
     {
@@ -134,21 +134,21 @@ class PrivateCarController extends Controller
         return $result;
     }
 
-    // public function approve(Request $request)
-    // {
-    //     $data = $request->all();
-    //     $id = $data['id'];
-    //     try {
-    //         $this->privateCarRepository->update($id, $data);
-    //         $result['status'] = ApiStatus::list_private_car_success_status;
-    //         $result['statusCode'] = ApiStatus::list_private_car_success_statusCode;
-    //         $result['message'] = 'Transaction approved successfully.';
-    //     } catch (\Exception $e) {
-    //         $result['status'] = ApiStatus::list_private_car_failed_status;
-    //         $result['errCode'] = ApiStatus::list_private_car_failed_statusCode;
-    //         $result['errDesc'] = ApiStatus::list_private_car_failed_Desc;
-    //         $result['message'] = $e->getMessage();
-    //     }
-    //     return $result;
-    // }
+    public function approve(Request $request)
+    {
+        $data = $request->all();
+        $id = $data['id'];
+        try {
+            $this->privateCarRepository->update($id, $data);
+            $result['status'] = ApiStatus::list_private_car_success_status;
+            $result['statusCode'] = ApiStatus::list_private_car_success_statusCode;
+            $result['message'] = 'Transaction approved successfully.';
+        } catch (\Exception $e) {
+            $result['status'] = ApiStatus::list_private_car_failed_status;
+            $result['errCode'] = ApiStatus::list_private_car_failed_statusCode;
+            $result['errDesc'] = ApiStatus::list_private_car_failed_Desc;
+            $result['message'] = $e->getMessage();
+        }
+        return $result;
+    }
 }
