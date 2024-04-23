29cd280a2e8f006014bbe1f9e91dcc9af600f314
RATCHAROTE ANGTHONG <68987790+Lechiana@users.noreply.github.com>
2024-04-23 11:49:14 +0700

    Edit API Update Private Car

--- app/Http/Controllers/APIs/PrivateCarController.php
+++ app/Http/Controllers/APIs/PrivateCarController.php
@@ -27,18 +27,10 @@ class PrivateCarController extends Controller
             $existing_car = $this->privateCarRepository->findBy($search_criteria);
 
             if ($existing_car) {
-                $update_data = [
-                    'car_category_id' => $data['car_category_id'],
-                    'car_registration' => $data['car_registration'],
-                    'car_brand' => $data['car_brand'],
-                    'car_color' => $data['car_color'],
-                    'record_status' => 2,
-                    // 'car_image' => $data['car_image'],
-                ];
                 if ($request->file('car_image')) {
-                    $update_data['car_image'] = save_image($request->file('car_image'), 500, '/images/content/privateCar/');
+                    $data['car_image'] = save_image($request->file('car_image'), 500, '/images/content/privateCar/');
                 }
-                $this->privateCarRepository->update($existing_car->id, $update_data);
+                $this->privateCarRepository->update($existing_car->id, $data);
             } else {
                 $save_data = [
                     'emp_id' => $data['emp_id'],
@@ -69,15 +61,39 @@ class PrivateCarController extends Controller
         return $result;
     }
 
-    public function deleteUpdate(Request $request)
+    public function update(Request $request)
     {
         $data = $request->all();
-        $id = $data['id'];
         try {
-            $save_data = [
-                'record_status' => 0
-            ];
-            $this->privateCarRepository->update($id, $save_data);
+
+            if ($request->file('car_image')) {
+                $data['car_image'] = save_image($request->file('car_image'), 500, '/images/content/privateCar/');
+            }
+            $this->privateCarRepository->update($data['id'], $data);
+
+            $result['status'] = ApiStatus::list_private_car_success_status;
+            $result['statusCode'] = ApiStatus::list_private_car_success_statusCode;
+            $result['listPrivateCar'] = 'Update Successfully.';
+        } catch (\Exception $e) {
+            $result['status'] = ApiStatus::list_private_car_error_statusCode;
+            $result['errCode'] = ApiStatus::list_private_car_error_status;
+            $result['errDesc'] = ApiStatus::list_private_car_errDesc;
+            $result['message'] = $e->getMessage();
+        }
+        return $result;
+    }
+
+    public function deleteUpdate(Request $request)
+    {
+        // $data = $request->all();
+        $id = $request->id;
+        // $id = $data['id'];
+        try {
+            // $save_data = [
+            //     'record_status' => 0
+            // ];
+            // $this->privateCarRepository->update($id, $save_data);
+            $this->privateCarRepository->delete($id);
 
             $result['status'] = ApiStatus::list_private_car_success_status;
             $result['statusCode'] = ApiStatus::list_private_car_success_statusCode;
@@ -118,21 +134,21 @@ class PrivateCarController extends Controller
         return $result;
     }
 
-    public function approve(Request $request)
-    {
-        $data = $request->all();
-        $id = $data['id'];
-        try {
-            $this->privateCarRepository->update($id, $data);
-            $result['status'] = ApiStatus::list_private_car_success_status;
-            $result['statusCode'] = ApiStatus::list_private_car_success_statusCode;
-            $result['message'] = 'Transaction approved successfully.';
-        } catch (\Exception $e) {
-            $result['status'] = ApiStatus::list_private_car_failed_status;
-            $result['errCode'] = ApiStatus::list_private_car_failed_statusCode;
-            $result['errDesc'] = ApiStatus::list_private_car_failed_Desc;
-            $result['message'] = $e->getMessage();
-        }
-        return $result;
-    }
+    // public function approve(Request $request)
+    // {
+    //     $data = $request->all();
+    //     $id = $data['id'];
+    //     try {
+    //         $this->privateCarRepository->update($id, $data);
+    //         $result['status'] = ApiStatus::list_private_car_success_status;
+    //         $result['statusCode'] = ApiStatus::list_private_car_success_statusCode;
+    //         $result['message'] = 'Transaction approved successfully.';
+    //     } catch (\Exception $e) {
+    //         $result['status'] = ApiStatus::list_private_car_failed_status;
+    //         $result['errCode'] = ApiStatus::list_private_car_failed_statusCode;
+    //         $result['errDesc'] = ApiStatus::list_private_car_failed_Desc;
+    //         $result['message'] = $e->getMessage();
+    //     }
+    //     return $result;
+    // }
 }
