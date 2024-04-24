d2c66ebe15c88a156006a23ab4da31a0a2e0aa24
RATCHAROTE ANGTHONG <68987790+Lechiana@users.noreply.github.com>
2024-04-24 12:00:29 +0700

    edit api PrivateCar

--- app/Http/Controllers/APIs/PrivateCarController.php
+++ app/Http/Controllers/APIs/PrivateCarController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Repositories\PrivateCarInterface;
 use Illuminate\Support\Facades\DB;
+use App\Models\Employee;
 
 
 
@@ -26,6 +27,8 @@ class PrivateCarController extends Controller
             ];
             $existing_car = $this->privateCarRepository->findBy($search_criteria);
 
+            $employeeData = Employee::find($data['emp_id']);
+
             if ($existing_car) {
                 if ($request->file('car_image')) {
                     // $data['car_image'] = save_image($request->file('car_image'), 500, '/images/content/privateCar/');
@@ -38,8 +41,8 @@ class PrivateCarController extends Controller
             } else {
                 $save_data = [
                     'emp_id' => $data['emp_id'],
-                    'company_id' => $data['company_id'],
-                    'department_id' => $data['department_id'],
+                    'company_id' => $employeeData->company_id,
+                    'department_id' => $employeeData->department_id,
                     'car_category_id' => $data['car_category_id'],
                     'car_registration' => $data['car_registration'],
                     'car_brand' => $data['car_brand'],
